f2b24b5c5e1b4233c2d8525d8b7769e3e828ffe9
baojame2 <baojame2@teach.cs.utoronto.ca>
2020-12-19 01:33:35 -0500

    implemented advection

--- include/advection.h
+++ include/advection.h
@@ -1,7 +1,12 @@
 //includes here
+#include <Eigen/Core>
+#include <Eigen/Geometry>
 
 //input
-
+//qdot - velocity (nx1)
+//q - position (nx1)
+//dt - time change
 //output
+//q updated position
 
-void advection();
+void advection(Eigen::VectorXd q, Eigen::VectorXd qdot, int dt);
--- src/advection.cpp
+++ src/advection.cpp
@@ -1,5 +1,5 @@
 #include "advection.h"
 
-void advection() {
-
+void advection(Eigen::VectorXd q, Eigen::VectorXd qdot, int dt) {
+	q = q + qdot * dt;
 }
