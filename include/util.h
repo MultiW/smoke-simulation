0f5ce3e7e65894f184889aca2c7c020b4c3f9546
baojame2 <baojame2@teach.cs.utoronto.ca>
2020-12-21 01:00:27 -0500

    flatten and unflatten utils

--- include/util.h
+++ include/util.h
@@ -3,6 +3,7 @@
 
 #include <Eigen/Core>
 
+#include "grid.h"
 #include <vector>
 
 void addToCol(Eigen::MatrixXd& matrix, int columnIndex, double value);
@@ -10,5 +11,9 @@ void addToCol(Eigen::MatrixXd& matrix, int columnIndex, double value);
 void addRows(Eigen::MatrixXd& matrix, const Eigen::MatrixXd& rows);
 
 void colToSortedVector(const Eigen::VectorXd& column, std::vector<double>& sortedVector);
+
+void flatten3d(Eigen::VectorXd& newArray, Grid &grid);
+
+void unflatten(Grid &grid, Eigen::VectorXd &vector);
 
 #endif
